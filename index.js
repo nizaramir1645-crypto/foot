8e4bfe210900206f5bf5424844684627ff25c099
nizaramir1645-crypto <nizaramir1645@gmail.com>
2026-05-03 15:57:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const fetch = require("node-fetch");
 
 const app = express();
 
-const TARGET = "https://cdndirector.dailymotion.com/cdn/live/video/x9yjnv4.m3u8?sec=5uuRnS5bAa15nENZkvS4D9wQ4WqxCQuGh_P8N7IANOCchtSD5r3mpgIPVmI_0YSgk0WeXKij5yC_ra-8AzpNrj942CdrzJo15WPKft7ZmsX8BJN6L-CVQf3HK182jdFu&dmTs=311689&dmV1st=99981483-72d8-5726-aecc-1566c7809082";
-
 app.get("*", async (req, res) => {
+  const target = "https://cdndirector.dailymotion.com/cdn/live/video/x9yjnv4.m3u8?sec=5uuRnS5bAa15nENZkvS4D9wQ4WqxCQuGh_P8N7IANOCchtSD5r3mpgIPVmI_0YSgk0WeXKij5yC_ra-8AzpNrj942CdrzJo15WPKft7ZmsX8BJN6L-CVQf3HK182jdFu&dmTs=311689&dmV1st=99981483-72d8-5726-aecc-1566c7809082";
+
   try {
-    const response = await fetch(TARGET, {
+    const response = await fetch(target, {
       headers: {
         "User-Agent": "Mozilla/5.0",
         "Referer": "https://www.dailymotion.com/"
@@ -15,7 +15,7 @@ app.get("*", async (req, res) => {
     });
 
     res.set("Access-Control-Allow-Origin", "*");
-    res.set("Content-Type", response.headers.get("content-type") || "application/vnd.apple.mpegurl");
+    res.set("Content-Type", "application/vnd.apple.mpegurl");
     response.body.pipe(res);
   } catch (err) {
     res.status(500).send("Error: " + err.message);
