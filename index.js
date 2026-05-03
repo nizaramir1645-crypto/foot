79f70d33dfa517ec8a5cf7cd5fdb8e995f915254
nizaramir1645-crypto <nizaramir1645@gmail.com>
2026-05-03 15:17:08 +0100

    Update base URL and adjust fetch target

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const fetch = require("node-fetch");
 
 const app = express();
 
+const BASE = "http://57.131.51.215/auth/ynB0vyJ8WO9D8XgqeZDbCZGDYzJ170vLmoiN7At9w2omH2IUMn2M6nWeozCykFhGznIwK7qqxk_ycVVldib8INnAA5to9360IXFWuN88KuXvQaAjQ7047BlgtEnyrH3bBsiJc0ba4Rz-SsJbcEpodrtwaHtIHa1d0JNLRGxdlgXcTFbfHtGHmeYSk_Y0sIl6ziMnhbQx2OLaXEnAHzrjU72DBENentK2Yyo3fyFi2sHj-uSIKUkSRjBwDgbR_IvKssgYJRRn7YPX45ZCY1QXpt8DXcxYOww8LO68p_efZelIAP2oMzV7Fljgb22UwW2LMesUsx0Se4woN4IFAJ9KiMbUspc0i4GYSXLHp6D3GEWholj0G7_VXwfzkWxzbTT3rleUD1hHcD15mD0cSfzB_cW0OsxzsFIKKK4kasvNHijvJiu14vkHl9vcb7OIJA-3v_uPZuoy5SLKxZ1v_bIfPAwa08o-hx-PTZKVuEERjXIFAuKVFVCmMFYRHoWJBE_-oRoBDjHPxYsz24Y_qbbx-nurk5kn9nICxiZ4SOtKagHTsOLIF3y9E-D1KKam5zIYHCNq7SSdRMUwv6jtt6m8GB4VhiwA27mmT3gBl3B_UWSqpkMnWe9lQAMjVmGkoAIe";
+
 app.get("*", async (req, res) => {
-  const base = "http://84.17.50.102/nbatv";
   const path = req.path === "/" ? "/index.m3u8" : req.path;
-  const target = base + path;
+  const target = BASE + path;
 
   try {
     const response = await fetch(target, {
       headers: {
         "User-Agent": "VLC/3.0.18 LibVLC/3.0.18",
-        "Referer": "http://84.17.50.102/"
+        "Referer": "http://57.131.51.215/"
       }
     });
 
