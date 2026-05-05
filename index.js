c71fcdb264972f156a38024c3188b9e55336f530
nizaramir1645-crypto <nizaramir1645@gmail.com>
2026-05-05 19:47:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const fetch = require("node-fetch");
-
 const app = express();
 
 app.get("*", async (req, res) => {
-  const target = "https://cdndirector.dailymotion.com/cdn/live/video/x9yjnv4.m3u8?sec=5uuRnS5bAa15nENZkvS4D9wQ4WqxCQuGh_P8N7IANOCchtSD5r3mpgIPVmI_0YSgk0WeXKij5yC_ra-8AzpNrj942CdrzJo15WPKft7ZmsX8BJN6L-CVQf3HK182jdFu&dmTs=311689&dmV1st=99981483-72d8-5726-aecc-1566c7809082";
-
+  const target = "http://atlan2025.me:80/Rochdi70sam/d3hm7lsqrh/534831";
   try {
     const response = await fetch(target, {
       headers: {
         "User-Agent": "Mozilla/5.0",
-        "Referer": "https://www.dailymotion.com/"
+        "Referer": "http://atlan2025.me/"
       }
     });
-
     res.set("Access-Control-Allow-Origin", "*");
     res.set("Content-Type", "application/vnd.apple.mpegurl");
     response.body.pipe(res);
